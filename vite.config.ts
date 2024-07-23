5beb0dea95441e5c42259de44b2823d3ad480732
Erik Sundberg <erik.emanuel.sundberg@outlook.com>
2024-07-23 21:50:51 +0200

    featt: set up gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,7 @@ import {VitePWA} from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    outDir: 'docs',
-  },
+  base: '/journalist/',
   plugins: [react(), VitePWA({
     registerType: 'autoUpdate',
     includeAssets: ['favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
