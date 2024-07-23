40f7b3b66344002ff4f1c7ac3e587090c6e4e55b
Erik Sundberg <erik.emanuel.sundberg@outlook.com>
2024-07-23 21:23:59 +0200

    feat: build to docs

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import {VitePWA} from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    outDir: 'docs',
+  },
   plugins: [react(), VitePWA({
     registerType: 'autoUpdate',
     includeAssets: ['favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
